509c12fd4ed5e90f41a211f1dc762217a9792dc9
student <student@0210-al-05135.dhcp.wustl.edu>
2022-11-17 13:32:08 -0600

    Studio

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,52 @@
 package studio8;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Date {
+	public int day;
+	public int month;
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, isHoliday, month, year);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && isHoliday == other.isHoliday && month == other.month && year == other.year;
+	}
+	public int year;
+	public boolean isHoliday;
 
-
-    public static void main(String[] args) {
-
-    }
-
+	public Date (int month, int day, int year, boolean isHoliday) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+		this.isHoliday = isHoliday;
+	}
+	public static String toString (Date D) {
+		String m = Integer.toString(D.month);
+		String d = Integer.toString(D.day);
+		String y = Integer.toString(D.year);
+		return d + "/" + m + "/" + y;
+	}	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is the day?");
+		int day = in.nextInt();
+		System.out.println("What is the month?");
+		int month = in.nextInt();
+		System.out.println("What is the year?");
+		int year = in.nextInt();
+		System.out.println("Is it a holiday?: true or false");
+		boolean isHoliday = in.nextBoolean();
+		Date T = new Date(day, month, year, isHoliday);
+		System.out.println(toString(T) + "\nTrue or False: Is the day a Holiday? " + isHoliday);
+	}
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,41 @@
 package studio8;
 
+import java.util.Scanner;
 
 public class Time {
-
+	private int hour;
+	private int minute;
+	private boolean is12or24hrFormat;
+	private boolean isPm;
+	public Time (int hour, int minute, boolean is12or24hrFormat) {
+		this.hour = hour;
+		this.minute = minute;
+		this.is12or24hrFormat = is12or24hrFormat;
+	}
+	public static String toString (Time T) {
+		String h = Integer.toString(T.hour);
+		String m = Integer.toString(T.minute);
+		return h + ":" + m;
+	}	
 	public static void main(String[] args) {
- 
-    	
-    }
-
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is the hour?");
+		int hour = in.nextInt();
+		System.out.println("What is the minute?");
+		int minute = in.nextInt();
+		System.out.println("write true if it's a 12 hr format or false if it's a 24 hr format?: true or false");
+		boolean is12or24hrFormat = in.nextBoolean();
+//		System.out.println("Write true if it's pm, false if it's am");
+//		boolean isPm = in.nextBoolean();
+//		if (is12or24hrFormat == true && isPm == true) {
+//			if (hour > 12) {
+//				hour -= 12;
+//			}
+//		}
+//		else if (is12or24hrFormat == false) {
+////			if (isPm == true && )
+//		}
+		Time T = new Time(hour, minute, is12or24hrFormat);
+		System.out.println(toString(T) + "\nTrue or False: Is the format 12 or 24 hours? " + is12or24hrFormat);
+	}
 }
